b8fe00996341b547c75d73f470201acd782bcfcd
Clorhexidinum <mkubekov@gmail.com>
2020-05-21 22:20:51 +0300

    Правки после проверки

--- gulpfile.js
+++ gulpfile.js
@@ -114,6 +114,7 @@ gulp.task("js", function () {
 
 gulp.task("copy", function () {
   return gulp.src([
+    "source/css/style.css",
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
     "source/*.ico"
